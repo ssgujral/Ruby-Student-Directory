e537aeeb4d20e216a6982317db151c37a52d5b54
samgujral <71089547+samgujral@users.noreply.github.com>
2022-06-28 18:41:52 +0200

    Directory created

---
+++ directory.rb
@@ -0,0 +1,8 @@
+puts "Students enrolled at Evil Academy"
+puts "**********"
+puts "Mike Myers"
+puts "Kim Jon Il"
+puts "The Grim Reaper"
+puts "Evil Clown from It!"
+puts "Joseph (Joe) Vissarionovich Stalin"
+
