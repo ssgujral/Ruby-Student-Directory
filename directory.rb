709ff54c5f8811bf7ccad29a7cf172ba55a673dc
samgujral <71089547+samgujral@users.noreply.github.com>
2022-06-28 22:04:33 +0200

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 
-#first, store all students and cohort a has
+#stores all student names and their months of enrollment (cohorts) as a hash table
 
 student_list = [
 
@@ -11,27 +11,57 @@ student_list = [
 
 ]
 
-#method gets names of enrolled students from the user
+#interactive_menu
+
+def interactive_menu
+  students = []
+  loop do
+
+    puts "1. Add students to the directory"
+    puts "2. List students in the directory"
+    puts "9. Exit"
+
+    selection = gets.chomp
+    
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      puts print_student_header
+      puts print(students)
+      puts print_enrollment_count(students)
+    when "9"
+      exit
+    else
+      puts "I do not understand that command. Please try again."
+      puts ""
+    end
+  end
+end
+
+
+
+#lets the user input names of enrolled students and their months of enrollment (cohorts)
 
 def input_students
   puts "Enter the names of all newly enrolled students."
   puts ""
   puts "To finish entering students, hit return twice."
   puts ""
-  puts "Enter student name:"
+  puts "Enter the new student's name:"
 
   #create a blank array
   students = []
   #get the first student name
   name = gets.chomp
 
-  puts "Enter student's cohort (enrollment month:"
+  puts "Enter the new student's cohort (month of enrollment):"
   cohort_month = gets.chomp
   
   while !name.empty? do
 
     if cohort_month.empty?
-      students << {name: student, cohort: :NotSpecified}
+      students << {name: name, cohort: :Unspecified}
 
     else
 
@@ -39,14 +69,14 @@ def input_students
     
     end
 
-    puts "There are a now a total of #{students.count} enrolled students."
     puts ""
-    puts "To finish enterting students, push return twice."
-    puts" Otherwise, please enter the next student's name:"
+    puts "There are a now a total of #{students.count} enrolled students in the directory."
     puts ""
+    puts "Press return twice to stop adding new students."
+    puts "Otherwise, please enter the next new student's name:"
     name = gets.chomp
     puts ""
-    puts "Enter their cohort (enrollment) month:"
+    puts "Enter the new student's cohort (month of enrollment):"
     cohort_month = gets.chomp
   end
 
@@ -94,9 +124,6 @@ def print_enrollment_count (list)
   puts "In total, there are #{list.count} evil students."
 end
 
-students = input_students
+interactive_menu
 
-puts print_student_header
-puts print(students)
 
-puts print_enrollment_count(students)
