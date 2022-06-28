e114b4cdb8373ac1956f7852a57cf22178b113d7
samgujral <71089547+samgujral@users.noreply.github.com>
2022-06-28 18:48:07 +0200

    Student number as variable

--- directory.rb
+++ directory.rb
@@ -1,14 +1,16 @@
 
+number_of_students = 5
+
 #prints list of students
 puts "Students enrolled at Evil Academy"
 puts "**********"
+puts ""
 puts "Mike Myers"
 puts "Kim Jon Il"
 puts "The Grim Reaper"
 puts "Evil Clown from It!"
 puts "Joseph (Joe) Vissarionovich Stalin"
+puts ""
 
 #prints total number of students
-puts "In total, there are"
-print 5
-puts " evil students."
+puts "In total, there are #{number_of_students} evil students."
